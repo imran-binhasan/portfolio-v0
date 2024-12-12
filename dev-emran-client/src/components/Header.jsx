d9f39998083d2fcefa5beb33918cd736aa12db0c
imran-binhasan <imranbinhasan.work@gmail.com>
2024-12-12 23:35:12 +0600

    Add banner section

--- dev-emran-client/src/components/Header.jsx
+++ dev-emran-client/src/components/Header.jsx
@@ -1,12 +1,12 @@
 
 const Header = () => {
     return (
-        <div className="max-w-7xl mx-auto border flex justify-between p-2">
+        <div className="max-w-7xl mx-auto flex justify-between p-3">
             <div>
-                <h3 className="text-2xl text-violet-500">Emran</h3>
+                <h3 className="text-3xl text-violet-500">Emran</h3>
             </div>
             <div>
-                <button className="border rounded-2xl border-violet-600 text-violet-600 px-3 py-1 hover:text-white hover:bg-violet-600 ">Say Hello</button>
+                <button className="border rounded-2xl border-violet-600 text-violet-600 px-4 py-1.5 hover:text-white hover:bg-violet-600 ">Say Hello</button>
             </div>
         </div>
     );
