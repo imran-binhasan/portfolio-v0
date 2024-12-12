5a2098c26518a0aebd4253c9a5289ddf6dc04e84
emran-binhasan <emranbinhasan.zero@gmail.com>
2024-12-12 22:26:25 +0600

    Add Header

---
+++ dev-emran-client/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import Banner from "../components/Banner";
+import Header from "../components/Header";
+
+const Home = () => {
+    return (
+        <div>
+            <Header/>
+            <Banner/>
+        </div>
+    );
+};
+
+export default Home;
